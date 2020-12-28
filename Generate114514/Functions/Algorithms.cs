d44231cd97f5a837fb10114b8283a2a7ab54d7dd
sselecirPyM <1151047603@qq.com>
2020-12-28 16:55:36 +0800

    加速114514

--- Generate114514/Functions/Algorithms.cs
+++ Generate114514/Functions/Algorithms.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -20,14 +21,34 @@ namespace Generate114514.Functions
             computationData.target = target;
             computationData.cancellationToken = cancellationToken;
 
+            computationData.forBranchCull = new HashSet<int>[sourceLength];
+            for (int i = 0; i < computationData.forBranchCull.Length; i++)
+            {
+                computationData.forBranchCull[i] = new HashSet<int>();
+            }
+            //computationData.forBranchCull = new SortedSet<int>[sourceLength];
+            //for(int i=0;i < computationData.forBranchCull.Length;i++)
+            //{
+            //    computationData.forBranchCull[i] = new SortedSet<int>();
+            //}
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Restart();
             if (_ComputeCancelableWithReport(0, 0, 0, computationData, 1, 0, ref Report))
             {
-                return string.Format("{0}={1}", ToInfix(operatorQueue, source), target);
+                stopwatch.Stop();
+                return string.Format("{0}={1}\nwith {2} milliseconds.", ToInfix(operatorQueue, source), target, stopwatch.ElapsedMilliseconds);
             }
             else if (cancellationToken.IsCancellationRequested)
-                return "compute was canceled.";
+            {
+                stopwatch.Stop();
+                return string.Format("compute was canceled.\nwith {0} milliseconds.", stopwatch.ElapsedMilliseconds);
+            }
             else
-                return "cannot find answer";
+            {
+                stopwatch.Stop();
+                return string.Format("cannot find answer.\nwith {0} milliseconds.", stopwatch.ElapsedMilliseconds);
+            }
         }
 
         static string ToInfix(RPNOperator[] operatorQueue, int[] source)
@@ -83,6 +104,8 @@ namespace Generate114514.Functions
             public RPNOperator[] operatorQueue;
             public int[] opValues;
             public CancellationToken cancellationToken;
+            public HashSet<int>[] forBranchCull;
+            //public SortedSet<int>[] forBranchCull;
         }
 
         static bool _Compute(int NumOfA, int NumOfB, int depth, ComputationData computationData)
@@ -131,29 +154,55 @@ namespace Generate114514.Functions
             return false;
         }
 
-        const int c_performaceValue = 13;//shrink can decrease performace.expand can increase the delay of cancellation.
+        const int c_performaceValue = 5;
         static bool _ComputeCancelableWithReport(int NumOfA, int NumOfB, int depth, ComputationData computationData, double weight, double baseValue, ref double Report)
         {
             ref var operatorQueue = ref computationData.operatorQueue;
+            ref var opValues = ref computationData.opValues;
+            ref var target = ref computationData.target;
             ref var cancellationToken = ref computationData.cancellationToken;
+            ref var forBranchCull = ref computationData.forBranchCull;
+
+            if (depth != 0 && NumOfA == NumOfB)
+            {
+                Stack<int> computeStack = new Stack<int>(opValues.Length);
+                Stack<int> sourcesStack = new Stack<int>(opValues);
+                computeStack.Push(sourcesStack.Pop());
+                for (int i = 0; i < depth; i++)
+                {
+                    if (operatorQueue[i] == RPNOperator.Push)
+                        computeStack.Push(sourcesStack.Pop());
+                    else if (operatorQueue[i] == RPNOperator.Add)
+                        computeStack.Push(computeStack.Pop() + computeStack.Pop());
+                    else if (operatorQueue[i] == RPNOperator.Sub)
+                        computeStack.Push(computeStack.Pop() - computeStack.Pop());
+                    else if (operatorQueue[i] == RPNOperator.Mul)
+                        computeStack.Push(computeStack.Pop() * computeStack.Pop());
+                    else if (operatorQueue[i] == RPNOperator.Div)
+                        throw new NotImplementedException();
+                }
+                int computeValue = computeStack.Peek();
+                if (!forBranchCull[depth >> 1].Add(computeValue))
+                {
+                    Report = baseValue + weight;
+                    return false;
+                }
+                long testValue = Math.Abs(computeValue);
+                int sourcesStackCount = sourcesStack.Count;
+                long testValue2 = 1;
+                for (int i = 0; i < sourcesStackCount; i++)
+                {
+                    testValue2 = Math.Max(Math.Abs(opValues[i]), 2) * testValue2;
+                }
+                if (testValue * testValue2 < Math.Abs(target))
+                    return false;
+                if (testValue - testValue2 > Math.Abs(target))
+                    return false;
+
+            }
+
             int taskComplete = 0;
             double subWeight = weight / (Convert.ToInt32(NumOfA < operatorQueue.Length) * 3 + Convert.ToInt32(NumOfA > NumOfB) * 3);
-
-            if (NumOfA < operatorQueue.Length >> 1)
-            {
-                operatorQueue[depth] = RPNOperator.Push;
-                if (operatorQueue.Length - depth < c_performaceValue)
-                {
-                    if (_Compute(NumOfA + 1, NumOfB, depth + 1, computationData)) return true;
-                    if (cancellationToken.IsCancellationRequested) return false;
-                }
-                else
-                {
-                    if (_ComputeCancelableWithReport(NumOfA + 1, NumOfB, depth + 1, computationData, subWeight * 3, baseValue, ref Report)) return true;
-                    taskComplete += 3;
-                    if (cancellationToken.IsCancellationRequested) return false;
-                }
-            }
             if (NumOfA > NumOfB)
             {
                 if (operatorQueue.Length - depth < c_performaceValue)
@@ -178,6 +227,21 @@ namespace Generate114514.Functions
                     operatorQueue[depth] = RPNOperator.Mul;
                     if (_ComputeCancelableWithReport(NumOfA, NumOfB + 1, depth + 1, computationData, subWeight, baseValue + subWeight * taskComplete, ref Report)) return true;
                     taskComplete++;
+                }
+            }
+            if (NumOfA < operatorQueue.Length >> 1)
+            {
+                operatorQueue[depth] = RPNOperator.Push;
+                if (operatorQueue.Length - depth < c_performaceValue)
+                {
+                    if (_Compute(NumOfA + 1, NumOfB, depth + 1, computationData)) return true;
+                    if (cancellationToken.IsCancellationRequested) return false;
+                }
+                else
+                {
+                    if (_ComputeCancelableWithReport(NumOfA + 1, NumOfB, depth + 1, computationData, subWeight * 3, baseValue + subWeight * taskComplete, ref Report)) return true;
+                    taskComplete += 3;
+                    if (cancellationToken.IsCancellationRequested) return false;
                 }
             }
             Report = baseValue + weight;
